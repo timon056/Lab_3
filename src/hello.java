01f7d75399b8646a5da6bd5ed5b84dabf9ea720a
Artem Bondarenko <timon-1010@yandex.ru>
2015-05-20 06:48:15 +0300

    new

--- src/hello.java
+++ src/hello.java
@@ -1,14 +1,17 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class hello {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Results of checking:");
         System.out.println(checkWithRegExp("_@BEST"));
-        System.out.println(checkWithRegExp("vovan"));
+        System.out.println(checkWithRegExp("vovano"));
         System.out.println(checkWithRegExp("vo"));
         System.out.println(checkWithRegExp("Z@OZA"));
+        System.out.println(checkWithRegExp("HELLO"));
+        System.out.println(checkWithRegExp("123"));
     }
-    public static boolean checkWithRegExp(String userNameString){
+
+    public static boolean checkWithRegExp(String userNameString) {
         Pattern p = Pattern.compile("^[az09]{3,15}$");
         Matcher m = p.matcher(userNameString);
         return m.matches();
